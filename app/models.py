03a13af240762fb4cd2928caafbb63cc9bd1a825
Stepan Kozurak <step.kozbvb@gmail.com>
2020-08-15 13:22:20 +0200

    Improved Event model

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from flask_login import UserMixin
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import relationship
 
 from . import login_manager
 
@@ -43,3 +44,14 @@ class User(Base, UserMixin):
     def __repr__(self):
         return "<User %r>" % self.email
 
+
+class Event(Base):
+    __tablename__ = 'event'
+
+    _id = Column(Integer, primary_key=True)
+    start_time = Column(DateTime, unique=False, nullable=False)
+    end_time = Column(DateTime, unique=False, nullable=False)
+    author_email = Column(String, ForeignKey('user.email'), nullable=False)
+    author = relationship('User', foreign_keys=[author_email])
+    subject = Column(String, unique=False, nullable=False)
+    description = Column(String, unique=False, nullable=False)
