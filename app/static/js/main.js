69e83540c633ff5ed9b830d2df7cf2eb4b9723c8
Stepan Kozurak <step.kozbvb@gmail.com>
2020-08-17 15:05:23 +0200

    Added new pages, styles, js file

---
+++ app/static/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    const hiddenElement = $('.hidden').css('display', 'none');
+    $('.continueButton').on('click', function () {
+        const subject = $('#subject');
+        const description = $('#description');
+        const start_time = $('#start_time');
+        const end_time = $('#end_time');
+
+        let subject_str = [];
+        let description_str = [];
+        let start_time_str = [];
+        let end_time_str = [];
+
+        subject_str.push(subject.val());
+        description_str.push(description.val());
+        start_time_str.push(start_time.val());
+        end_time_str.push(end_time.val());
+
+
+        const request = $.ajax({
+            url: "/subject_description",
+            type: "POST",
+            data: {
+                subject: subject_str[0],
+                description: description_str[0],
+                start_time: start_time_str[0],
+                end_time: end_time_str[0]
+            },
+        });
+
+        request.done(function (data) {
+            $('.show').fadeOut("slow", function () {
+                $(this).css('display', 'none')
+                $(hiddenElement).fadeIn("slow", function () {
+                    $(this).css('display', 'flex')
+                });
+            });
+        });
+    });
+    // const contentListElement = $('.content_events_list').length;
+    // if (contentListElement !== 0) {
+    //     $('.content_body_right_title').remove();
+    //     $('.content_cards').remove();
+    // }
+});
